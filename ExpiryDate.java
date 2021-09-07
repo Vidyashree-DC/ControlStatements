008efcaa633a53e6889fec40287b16047c3c80e6
Vidyashree-DC <89985657+Vidyashree-DC@users.noreply.github.com>
2021-09-07 14:35:57 +0530

    Initial commit

---
+++ ExpiryDate.java
@@ -0,0 +1,13 @@
+package com.Xworkz.ControlStatement;
+
+public class ExpiryDate {
+	public static void main(String[] args){
+		short YearofManufactured = 2017;
+		short price = 3000;
+		if (YearofManufactured >= 2019 && price <= 3000){
+		System.out.println(" Continue Shopping");
+		}else {
+			System.out.println("Sopping Denied");
+		}
+	}
+}
